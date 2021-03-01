855c2511f8bdd44068d76e80282da630df35046d
Matin <matinsoltany2000@gmail.com>
2021-03-01 09:53:32 +0300

    Collecting static files

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_data = models.DateTimeField()
+    pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to="images/")
 
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Blog
+
+def blogs(req):
+    blogs = Blog.objects
+    return render(req, "blogs.html",{"blogs":blogs})
+def detail(req,blog_id):
+    detail_blog = get_object_or_404(Blog,pk = blog_id)
+    return render(req,'detail.html',{'blog':detail_blog})
